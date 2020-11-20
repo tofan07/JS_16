241f19c827c1ad2264137f561680c3183f11fa77
tofan07 <tofan0797@gmail.com>
2020-11-20 20:04:13 +0300

    Refact script.js

--- lesson02_hard/script.js
+++ lesson02_hard/script.js
@@ -1,28 +1,20 @@
 'use strict';
 // Создаем переменную с целым числом
 let num = 266219;
-// Разбиваем число на массив строк
-let numsArr = ('' + num).split('');
-// Создаем пустой массив
-let intArr = [];
-// С помощью цикла преобразуем строки в числа
-for (let i = 0; i <= numsArr.length - 1; i++) {
-  intArr.push(parseInt(numsArr[i]));
+// Преобразуем в строку
+num = num.toString();
+let total = 1;
+// Перемножаем
+for (let item of num) {
+  total *= item;
 }
-
-// С помощью метода перебора reduce перемножаем элементы массива
-const result = intArr.reduce(function(mult, current) {
-  return mult * current;
-}, 1);
-// Выводим результат
-console.log( result );
+//Выводим результат
+console.log(total);
 
 // Возводим результат в степень 3
-let exp = result;
-exp **= 3;
-console.log(exp);
-
+total **= 3;
+console.log(total);
+//Преобразуем в строку
+total = total.toString();
 // Выводим первые две цифры полученного результата
-let expResultArr = exp.toString();
-console.log(expResultArr.slice(0, 2));
-
+console.log(total.slice(0, 2));
