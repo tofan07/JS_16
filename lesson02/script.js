ec2cc2769b7c1ed3285bf56d52d9b2baa9e486e0
tofan07 <tofan0797@gmail.com>
2020-11-20 18:47:56 +0300

    added fix in script.js

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,45 +1,23 @@
-let  money,
-    income,
-    addExpenses,
-    deposit,
-    mission, 
-    period;
+let  money = 60000,
+    income = 'кураторство',
+    addExpenses = 'Топливо, Продукты, Связь',
+    deposit = true,
+    mission = 120000, 
+    period = 12;
 
-// Задание 1.1
-money = 60000;
 console.log('money: ', money);
-
-// Задание 1.2
-income = 'кураторство';
 console.log('income: ', income);
-
-// Задание 1.3
-addExpenses = 'Топливо, Продукты, Связь';
 console.log('addExpenses: ', addExpenses);
-
-// Задание 1.4
-deposit = true;
 console.log('deposit: ', deposit);
-
-// Задание 1.5
-mission = 120000;
 console.log('mission: ', mission);
-
-// Задание 1.6
-period = 12;
 console.log('period: ', period);
 
 // Задание 2.1
 console.log('type of "money": ', typeof(money));
 console.log('type of "income": ', typeof(income));
 console.log('type of "deposit": ', typeof(deposit));
-
-// Задание 2.2
 console.log('length of string addExpenses: ', addExpenses.length);
-
-// Задание 2.3
-console.log(`Период равен ${period} месяцам`);
-console.log(`Цель заработать ${mission} рублей`);
+console.log(`Период равен ${period} месяцам, цель заработать ${mission} рублей`);
 
 // Задание 2.4
 let strExpenses = addExpenses.toLowerCase();
