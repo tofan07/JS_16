53d4ab88014ea8c9ea04edb774d745ea8bbe3888
tofan07 <tofan0797@gmail.com>
2020-11-23 18:48:32 +0300

    Added lesson03_hard

---
+++ lesson03_hard/script.js
@@ -0,0 +1,45 @@
+'use strict';
+// Задание 1
+let lang = 'enw';
+let days,
+    daysRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    daysEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// через if
+  if (lang === 'ru') {
+    days = daysRu;
+  } else if (lang === 'en') {
+    days = daysEn;
+  } else {
+    days = 'Неверное значение';
+  }
+console.log(days);
+
+// через switch case
+switch (lang) {
+  case 'ru':
+    days = daysRu;
+    break;
+  case 'en':
+    days = daysEn;
+    break;
+  default:
+    days = 'Неверное значение';
+}
+console.log(days);
+
+// Через многомерный массив
+days = {
+  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  en: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+};
+console.log(days[lang]);
+
+// Задание 2
+let namePerson = 'Иван';
+
+let rank = (namePerson === 'Артем') ? 'Директор' : ((namePerson === 'Максим') ? 'Преподаватель' : 'Студент');
+console.log('rank: ', rank);
+
+
+
