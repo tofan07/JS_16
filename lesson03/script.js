5f0aac67bd36c0e3471c29bb3fdbcb95bad749bc
tofan07 <tofan0797@gmail.com>
2020-11-23 20:02:07 +0300

    refact script.js

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,45 +1,42 @@
 'use strict';
-let  money = 60000,
+let money,
     income = 'кураторство',
     addExpenses = 'Топливо, Продукты, Связь',
     deposit = true,
     mission = 120000, 
-    period = 6;
+    period = 6,
+    expenses1 = '',
+    amount1 = '',
+    expenses2 = '',
+    amount2 = '',
+    budgetMonth = '',
+    budgetDay = '';
 
-// console.log('money: ', money);
-// console.log('income: ', income);
-// console.log('addExpenses: ', addExpenses);
-// console.log('deposit: ', deposit);
-// console.log('mission: ', mission);
-// console.log('period: ', period);
-// Задание 2.1
 console.log(typeof(money));
 console.log(typeof(income));
 console.log(typeof(deposit));
 console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцам,\nцель заработать ${mission} рублей`);
-// Задание 2.4
-// let strExpenses = addExpenses.toLowerCase();
-// console.log(strExpenses.split(', '));
-// Задание 2.5
-let budgetDay = money / 30;
-// //Задание 2.6
-// console.log('budgetDay: ', budgetDay);
 
-// Урок №3
-money = prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.').split(', ');
+money = prompt('Ваш месячный доход?', 60000);
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.',
+ 'Лицензионное ПО, Связь, Кино').split(', ');
 console.log(addExpenses);
+
 deposit = confirm('Есть ли у вас депозит в банке?');
 
-let expenses1 = prompt('Введите обязательную статью расходов.'),
-    expenses2 = prompt('Введите обязательную статью расходов.'),
-    amount1 = +prompt('Во сколько это обойдется?'),
-    amount2 = +prompt('Во сколько это обойдется?'),
-    budgetMonth = amount1 + amount2;
+expenses1 = prompt('Введите обязательную статью расходов.', 'Коммунальные платежи');
+amount1 = +prompt('Во сколько это обойдется?', 20000);
 
+expenses2 = prompt('Введите обязательную статью расходов.', 'Бензин');
+amount2 = +prompt('Во сколько это обойдется?', 10000);
+
+budgetMonth = amount1 + amount2;
 console.log('Бюджет на месяц: ', budgetMonth);
+
 console.log('Цель будет достигнута за:', Math.ceil(mission / budgetMonth), 'месяцев');
+
 budgetDay = budgetMonth / 30;
 console.log('Бюджет на день: ', Math.floor(budgetDay));
 
