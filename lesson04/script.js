1328f028ab538fca095bbfeb4d702c94d1c8014c
tofan07 <tofan0797@gmail.com>
2020-11-24 14:32:00 +0300

    Refact lesson04/script.js

--- lesson04/script.js
+++ lesson04/script.js
@@ -2,34 +2,17 @@
 let money = +prompt('Ваш месячный доход?', 60000),
     income = 'кураторство',
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.',
-                          'Лицензионное ПО, Связь, Кино'),
+                  'Лицензионное ПО, Связь, Кино'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 120000, 
     period = 6,
-    expenses1 = '',
-    expenses2 = '',
-    amount1 = '',
-    amount2 = '',
+    expenses1 = prompt('Введите обязательную статью расходов.', 'Коммунальные платежи'),
+    amount1 = +prompt('Во сколько это обойдется?', '18300'),  
+    expenses2 = prompt('Введите обязательную статью расходов.', 'Бензин'),
+    amount2 = +prompt('Во сколько это обойдется?', '15000'),
     budgetDay = '',
-    accumulatedMonth = 0,
+    accumulatedMonth = '',
     showTypeOf = function(data) {console.log(typeof(data));};
-
-/* Не совсем понял должны ли передаваться параметры в функцию, поэтому сделал 2
-варианта, оба работают. */
-
-// Вариант 1. Параметры передаются в функцию при вызове
-
-// function getExpensesMonth(a, b){return a + b;}
-// function getAccumulatedMonth (a, b){return a - b;}
-// function getTargetMonth (a, b) {return Math.round(a / b);}
-// accumulatedMonth = getAccumulatedMonth(money, (getExpensesMonth(amount1, amount2)));
-
-// Вариант 2. В функцию изначально занесены переменные
-
-function getExpensesMonth() {return amount1 + amount2;}
-function getAccumulatedMonth() {return money - getExpensesMonth();}
-function getTargetMonth() {return Math.round(money / getExpensesMonth());}
-accumulatedMonth = getAccumulatedMonth();
 
 function getStatusIncome() {
   switch (true) {
@@ -50,25 +33,33 @@ function getStatusIncome() {
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
-console.log(addExpenses.split(', '));
-console.log(`Период равен ${period} месяцам,\nцель заработать ${mission} рублей`);
 
-expenses1 = prompt('Введите обязательную статью расходов.', 'Коммунальные платежи');
-amount1 = +prompt('Во сколько это обойдется?', '18300');
-expenses2 = prompt('Введите обязательную статью расходов.', 'Бензин');
-amount2 = +prompt('Во сколько это обойдется?', '15000');
+/* Не совсем понял должны ли передаваться параметры в функцию, поэтому сделал 2
+варианта, оба работают. */
 
-// Вызов для варианта 1
-// console.log(getTargetMonth(mission, accumulatedMonth));
+// Вариант 1. Параметры передаются в функцию при вызове
 
-// Вызов для варианта 2
-console.log(accumulatedMonth);
-console.log(getTargetMonth());
+function getExpensesMonth(a, b){return a + b;}
+function getAccumulatedMonth (a, b){return a - b;}
+function getTargetMonth (a, b) {return Math.ceil(a / b);}
+
+accumulatedMonth = getAccumulatedMonth(money, (getExpensesMonth(amount1, amount2)));
+console.log(`Расходы за месяц: ${getExpensesMonth(amount1, amount2)} руб.`);
+console.log('Список возможных расходов:', addExpenses.split(', '));
+console.log(`Цель может быть достигнута через ${getTargetMonth(mission, accumulatedMonth)} мес.`);
+
+// Вариант 2. В функцию изначально занесены переменные
+
+// function getExpensesMonth() {return amount1 + amount2;}
+// function getAccumulatedMonth() {return money - getExpensesMonth();}
+// function getTargetMonth() {return Math.ceil(mission / accumulatedMonth);}
+
+// accumulatedMonth = getAccumulatedMonth();
+// console.log(`Расходы за месяц: ${getExpensesMonth()} руб.`);
+// console.log('Список возможных расходов:', addExpenses.split(', '));
+// console.log(`Цель может быть достигнута через ${getTargetMonth()} мес.`);
 
 budgetDay = Math.ceil(accumulatedMonth / 30);
-console.log('Бюджет на день: ', budgetDay);
+console.log('Бюджет на день: ' + budgetDay + ' руб.');
+
 getStatusIncome();
-
-
-
-
