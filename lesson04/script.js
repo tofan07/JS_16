e04021757dcc64a92fd174f5fc82975b31cbb0b8
tofan07 <tofan0797@gmail.com>
2020-11-24 18:58:16 +0300

    Refact lesson04/script.js

--- lesson04/script.js
+++ lesson04/script.js
@@ -2,17 +2,37 @@
 let money = +prompt('Ваш месячный доход?', 60000),
     income = 'кураторство',
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.',
-                  'Лицензионное ПО, Связь, Кино'),
+                          'Лицензионное ПО, Связь, Кино'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 120000, 
     period = 6,
     expenses1 = prompt('Введите обязательную статью расходов.', 'Коммунальные платежи'),
     amount1 = +prompt('Во сколько это обойдется?', '18300'),  
     expenses2 = prompt('Введите обязательную статью расходов.', 'Бензин'),
-    amount2 = +prompt('Во сколько это обойдется?', '15000'),
-    budgetDay = '',
-    accumulatedMonth = '',
-    showTypeOf = function(data) {console.log(typeof(data));};
+    amount2 = +prompt('Во сколько это обойдется?', '15000');
+
+let showTypeOf = function(data) {
+  console.log(typeof(data));
+};
+// 1
+function getExpensesMonth(amountExp1, amountExp2){
+  return amountExp1 + amountExp2;
+}
+// 2
+function getAccumulatedMonth (moneyValue, expMonth){
+  return moneyValue - expMonth;
+}
+// 3
+let accumulatedMonth = getAccumulatedMonth(money, (
+  getExpensesMonth(amount1, amount2)
+  )
+);
+// 4
+function getTargetMonth (missionValue, accumulatedValue) {
+  return Math.ceil(missionValue / accumulatedValue);
+}
+// 6
+let budgetDay = Math.ceil(accumulatedMonth / 30);
 
 function getStatusIncome() {
   switch (true) {
@@ -29,37 +49,14 @@ function getStatusIncome() {
       console.log('Что-то пошло не так!');
   }
 }
-
+// 7
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-/* Не совсем понял должны ли передаваться параметры в функцию, поэтому сделал 2
-варианта, оба работают. */
-
-// Вариант 1. Параметры передаются в функцию при вызове
-
-function getExpensesMonth(a, b){return a + b;}
-function getAccumulatedMonth (a, b){return a - b;}
-function getTargetMonth (a, b) {return Math.ceil(a / b);}
-
-accumulatedMonth = getAccumulatedMonth(money, (getExpensesMonth(amount1, amount2)));
 console.log(`Расходы за месяц: ${getExpensesMonth(amount1, amount2)} руб.`);
 console.log('Список возможных расходов:', addExpenses.split(', '));
 console.log(`Цель может быть достигнута через ${getTargetMonth(mission, accumulatedMonth)} мес.`);
-
-// Вариант 2. В функцию изначально занесены переменные
-
-// function getExpensesMonth() {return amount1 + amount2;}
-// function getAccumulatedMonth() {return money - getExpensesMonth();}
-// function getTargetMonth() {return Math.ceil(mission / accumulatedMonth);}
-
-// accumulatedMonth = getAccumulatedMonth();
-// console.log(`Расходы за месяц: ${getExpensesMonth()} руб.`);
-// console.log('Список возможных расходов:', addExpenses.split(', '));
-// console.log(`Цель может быть достигнута через ${getTargetMonth()} мес.`);
-
-budgetDay = Math.ceil(accumulatedMonth / 30);
 console.log(`Бюджет на день: ${budgetDay} руб.`);
 
 getStatusIncome();
